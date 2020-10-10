8a4ce87afe1bcae216860bdd027d22dbbc9a93a7
Trung Doan <doanptand@gmail.com>
2020-10-10 19:43:53 +0700

    use sqlite

---
+++ app/src/main/java/com/t3h/storage/StudentActivity.java
@@ -0,0 +1,24 @@
+package com.t3h.storage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class StudentActivity extends AppCompatActivity {
+    private StudentManager studentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student);
+        studentManager = new StudentManager(this);
+        studentManager.addStudent(new Student(1, "Doan", 15));
+        studentManager.addStudent2(new Student(3, "Hung", 35));
+        List<Student> arrStudents = studentManager.getAllStudents();
+        Log.d("doanpt", "size=" + arrStudents.size());
+
+    }
+}
--- app/src/main/java/com/t3h/storage/StudentManager.java
+++ app/src/main/java/com/t3h/storage/StudentManager.java
@@ -2,9 +2,14 @@ package com.t3h.storage;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 import com.t3h.storage.db.StudentHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StudentManager {
 
@@ -32,6 +37,80 @@ public class StudentManager {
         ContentValues values = new ContentValues();
         values.put(StudentHelper.COL_NAME, student.getName());
         values.put(StudentHelper.COL_AGE, student.getAge());
-        db.insert(StudentHelper.STUDENT_TB,null, values);
+        db.insert(StudentHelper.STUDENT_TB, null, values);
     }
+
+    public List<Student> getAllStudents() {
+        List<Student> result = new ArrayList<>();
+        db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + StudentHelper.STUDENT_TB, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int idIndex = cursor.getColumnIndex(StudentHelper.COL_ID);
+                int nameIndex = cursor.getColumnIndex(StudentHelper.COL_NAME);
+                int ageIndex = cursor.getColumnIndex(StudentHelper.COL_AGE);
+                int id = cursor.getInt(idIndex);
+                String name = cursor.getString(nameIndex);
+                int age = cursor.getInt(ageIndex);
+                Student student = new Student(id, name, age);
+                result.add(student);
+            }
+        }
+        return result;
+    }
+
+    public List<Student> getAllStudent2() {
+        List<Student> result = new ArrayList<>();
+        db = helper.getReadableDatabase();
+        Cursor cursor = db.query(StudentHelper.STUDENT_TB,
+                null, null, null, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int idIndex = cursor.getColumnIndex(StudentHelper.COL_ID);
+                int nameIndex = cursor.getColumnIndex(StudentHelper.COL_NAME);
+                int ageIndex = cursor.getColumnIndex(StudentHelper.COL_AGE);
+                int id = cursor.getInt(idIndex);
+                String name = cursor.getString(nameIndex);
+                int age = cursor.getInt(ageIndex);
+                Student student = new Student(id, name, age);
+                result.add(student);
+            }
+        }
+        return result;
+    }
+
+    public void updateStudent(Student student) {
+        String sql = "UPDATE " + StudentHelper.STUDENT_TB + " SET "
+                + StudentHelper.COL_NAME + "='" + student.getName() + "',"
+                + StudentHelper.COL_AGE + "=" + student.getAge()
+                + " WHERE " + StudentHelper.COL_ID + "=" + student.getId();
+        db = helper.getWritableDatabase();
+        db.execSQL(sql);
+    }
+
+    public void updateStudent2(Student student) {
+
+        db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(StudentHelper.COL_NAME, student.getName());
+        values.put(StudentHelper.COL_AGE, student.getAge());
+        db.update(StudentHelper.STUDENT_TB, values,
+                StudentHelper.COL_ID + "=?",
+                new String[]{String.valueOf(student.getId())});
+    }
+
+    public void deleteStudent(int id) {
+        String sql = "DELETE FROM " + StudentHelper.STUDENT_TB
+                + " WHERE " + StudentHelper.COL_ID + "=" + id;
+        db = helper.getWritableDatabase();
+        db.execSQL(sql);
+    }
+
+    public void deleteStudent2(int id) {
+        db = helper.getWritableDatabase();
+        db.delete(StudentHelper.STUDENT_TB,
+                StudentHelper.COL_ID+ "=?",
+                new String[]{String.valueOf(id)});
+    }
+
 }
